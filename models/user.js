e03fed8580b5247eaf187e903764c5abfeb149cc
SUVAJITKARMAKAR <suvajit2001k@gmail.com>
2024-10-19 13:33:59 +0530

    SUVAJITKARMAKAR | BLOGGER | Password encryption and salt function created

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 // MODELS > user.js model 
 import { Schema, model } from "mongoose";
+import { createHmac, randomBytes } from "crypto";
 const userSchema = new Schema({
     fullname : {
         type : String,
@@ -28,6 +29,18 @@ const userSchema = new Schema({
         default : "USER"
     }
 }, { timestamps : true });
+
+// Password encryption 
+userSchema.pre('save', function(next) {
+    const user = this;
+    if ( !user.isModified("password") ) return;
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt).update(user.password).digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+    next();
+})
 
 // Model for the Schema definition
 const User = model("users", userSchema);
